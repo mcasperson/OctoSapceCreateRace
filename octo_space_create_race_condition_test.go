87e4515cc2763ae5a1a8d82b969ec687b2f9d4dc
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-06 07:47:45 +1000

    Added workflow file

--- octo_space_create_race_condition_test.go
+++ octo_space_create_race_condition_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
 	"github.com/mcasperson/OctoSapceCreateRace/internal/test"
 	"path/filepath"
@@ -9,8 +10,13 @@ import (
 
 func TestCreateSpaceAndUseIt(t *testing.T) {
 	testFramework := test.OctopusContainerTest{}
-	testFramework.ArrangeTest(t, func(t *testing.T, container *test.OctopusContainer, client *client.Client) error {
-		_, err := testFramework.Act(t, container, filepath.Join("test", "terraform", "2-usenewspace"), []string{})
-		return err
-	})
+
+	for i := 0; i < 10; i++ {
+		t.Log("Attempt " + fmt.Sprint(i+1))
+
+		testFramework.ArrangeTest(t, func(t *testing.T, container *test.OctopusContainer, client *client.Client) error {
+			_, err := testFramework.Act(t, container, filepath.Join("test", "terraform", "2-usenewspace"), []string{})
+			return err
+		})
+	}
 }
